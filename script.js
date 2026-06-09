2d49c02fc42899de5a06d7f1923ea7c6ba3cb792
ainnurkhadijahh <ainnurkhadijahh@gmail.com>
2026-06-09 14:24:40 +0800

    Add files via upload
    Initial project setup and homepage creation
    Updated README documentation
    Created homepage layout

---
+++ script.js
@@ -0,0 +1,13 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")) {
+        darkModeBtn.textContent = "☀ Light Mode";
+    } else {
+        darkModeBtn.textContent = "🌙 Dark Mode";
+    }
+
+});
